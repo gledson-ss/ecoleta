8fb99fb29dc8d2695c4dc60635ecba0c741817c1
GLEDSONS831 <glegogle84@gmail.com>
2020-06-03 08:38:28 -0300

    :pencil2: add function in backend

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,11 +1,21 @@
 import express from 'express'
 
+import PointController from './controllers/PointController'
+import ItemsControllers from './controllers/ItemsControllers'
+
 const routes = express.Router();
 
 
+const pointController = new PointController()
+const itemsControllers = new ItemsControllers()
 
-routes.get('/', (req, res) =>{
-    return res.json({ message: 'hello' })
-})
+routes.get('/items', itemsControllers.index)
+
+routes.post('/points', pointController.create)
+routes.get('/points', pointController.index)
+routes.get('/points/:id', pointController.show)
+
+
+
 
 export default routes;
